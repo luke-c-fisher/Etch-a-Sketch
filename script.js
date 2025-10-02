d89102603cf0665f9f41bdf25bef83466437fcca
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-02 15:05:24 -0400

    Create js function to create divs dynamically

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const container = document.getElementById("container")
+const container = document.getElementsByClassName("container");
+
+function createDiv(){
+    const divs = document.createElement("div");
+    divs.textContent = "I am a div";
+
+
+    container.appendChild(divs);
+}
+
+
+
+
 
