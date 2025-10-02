6757b68fd5cdb469eb516ed816233c814ec9080b
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-02 15:37:13 -0400

    Add flexbox properties to js element to text dynamism

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const container = document.getElementById("container");
 
 
-const divs = document.createElement("div");
-divs.setAttribute ("style", "color: black; background: pink; border: 2px black;");
-divs.textContent = "I am a div";
+const parentDiv = document.createElement("div");
+parentDiv.style.display = "flex";
+parentDiv.style.background = "pink";
+parentDiv.textContent = "parent div";
 
-container.appendChild(divs);
+
+
+
+container.appendChild(parentDiv);
+
 
 
 
