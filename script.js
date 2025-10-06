67c8f0cc2105614471bd810fa4c890f4d2806e16
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-06 11:04:11 -0400

    Add a background and color to the grid to make it look nice

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const grid = 256
 for (let i = 0; i < grid; i++){
     const divs = document.createElement("div");
     divs.id = "divs";
-    divs.style.background = "pink";
     divs.textContent = "child divs";
 
     container.appendChild(divs);
