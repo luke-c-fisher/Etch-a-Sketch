b642c975280ad8b87f09bca25107ae032dd7fb72
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-06 11:50:23 -0400

    Change name of divs and remove flex argument from divs

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ const grid = 256
 for (let i = 0; i < grid; i++){
     const divs = document.createElement("div");
     divs.id = "divs";
-    divs.textContent = "child divs";
-
+    divs.textContent = "divs";
     container.appendChild(divs);
 }
 
