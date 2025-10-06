ae1de043f8bada6c5b8d94dddf5b333069eb8b92
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-06 12:54:04 -0400

    Create an event listener that turns each div into a different color as the mouse hovers over it

--- script.js
+++ script.js
@@ -8,8 +8,16 @@ for (let i = 0; i < grid; i++){
     const divs = document.createElement("div");
     divs.id = "divs";
     divs.textContent = "divs";
+
+    divs.addEventListener("mouseover", function(){
+    divs.style.backgroundColor = "blue";
+    divs.style.color = "white";
+    });
+
     container.appendChild(divs);
 }
+
+
 
 
 
