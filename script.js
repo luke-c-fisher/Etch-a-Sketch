5a2710e9f40738a435aee38b70fa76bfc8a9ec8d
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-06 10:42:45 -0400

    Create grid of child divs and use container element to enable flexbox

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const container = document.getElementById("container");
 
 
 // USE FLEXBOX WITH JS TO CREATE DYNAMIC DIVS
-// const grid = 16
+const grid = 256
 
-// for (let i = 0; i < grid; i++){
-const divs = document.createElement("div");
-divs.id = "divs";
-divs.style.background = "pink";
-divs.textContent = "parent div";
+for (let i = 0; i < grid; i++){
+    const divs = document.createElement("div");
+    divs.id = "divs";
+    divs.style.background = "pink";
+    divs.textContent = "child divs";
 
-container.appendChild(divs);
-// }
+    container.appendChild(divs);
+}
 
 
 
